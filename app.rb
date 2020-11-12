85b12473f733d66f4f6247c794392d051499a5dd
Andrew Sloan <sloancodes@gmail.com>
2020-11-12 11:16:30 -0700

    worked through menu class

--- app.rb
+++ app.rb
@@ -4,3 +4,21 @@ menu_test = Menu.new
 
 puts menu_test.class
 
+menu = {
+  message: "Please Select an option"
+  options: ["option1", "option2", "exit"]
+}
+
+menu1 = {
+  message: "Please Select an option"
+  options: ["option1", "option2", "exit"]
+}
+
+menu2 = {
+  message: "Please Select an option"
+  options: ["option1", "option2", "exit"]
+}
+
+menu_test = Menu.new(menu)
+menu_test.print
+puts menu_test.class
--- menu.rb
+++ menu.rb
@@ -1,4 +1,14 @@
 class Menu
-  def Initialize
+  def Initialize(menu)
+    @menu = menu
   end
+  def print
+    puts @menu
+    #display menu message:
+    puts @menu[:message]
+#loop over menu option, need to add logic to add as well
+@menu[:options].each do |item|
+  puts "#{index + 1})" "#{item}"
+ end
+end
 end
